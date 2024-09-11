d3a4cb0ef67af131e606cae7678ac21c56adc83c
Pavel Filin <pavel164rus@gmail.com>
2024-09-11 23:14:40 +0400

    added sucinic_acid

--- util/FreqReader.py
+++ util/FreqReader.py
@@ -1,6 +1,21 @@
 import re
 import pandas as pd
 
+
+def read_freq_jdx(path, reverse=False):
+    freq = []
+    file = open(path, "r")
+    i = 0
+    for line in file:
+        if line.__len__ != 0 and line[0] != '#' and line[0] != '\n' and line[0].isdigit():
+            numbers = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", line)
+            freq.append([float(numbers[0]), 1-float(numbers[1]), 1-float(numbers[2])] )
+    file.close()
+    if reverse:
+        freq.reverse()
+    df = pd.DataFrame(freq, columns=["X", "Y", "Y2"])
+    return df
+
 def read_freq(path):
     freq = []
     file = open(path, "r")
